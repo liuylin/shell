5867e89a7432149d24016e6552fbe4959e386d4d
liuylin <1272894923@qq.com>
2018-02-25 02:10:45 -0800

    test.sh

---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#ret=$(date +%Y:%m:%d)
+
+#echo "time is: $ret"
+
+#echo "time is:`date +%Y:%m:%d`"
+
+#a=10
+#b=3
+
+#(( c = a + b ))
+#echo $c
+
+#(( c = a - b ))
+#echo $c
+
+#(( c = a * b ))
+#echo $c
+
+#(( c = a / b ))
+#echo $c
+
+#(( c = a % b ))
+#echo $c
+
+#c=$(( a + b ))
+#echo $c
+
+#pwd
+#cd ..
+#pwd
+
+#myint=120
+#mychar='c'
+#mydouble=3.14
+#mystring="helloworld"
+
+#echo $myint
+#echo $mychar
+#echo $mydouble
+#echo $mystring
+
+#echo $myint$mychar$mydouble$mystring
+
+
+#echo $myint$mychar$mydouble${mystring}aaaaaaaaaaa
+
+vale=100
+
+echo "#############1###############"
+echo "hello world : \` : \" : \\ : $val : $(date +%Y)"
+echo "#############2###############"
+echo 'hello world : \` : \" : \\ : $val : $(date +%Y)'
